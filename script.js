74fe4841e6e12aea55aef590da3e05f61f4c5ea7
myinan <m.yasir.inan@gmail.com>
2023-11-05 22:34:15 +0300

    Initiliaze the project files

---
+++ script.js
@@ -0,0 +1,12 @@
+const myLibrary = [];
+
+const Book = function(author, title, pages, read) {
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+    this.read = Boolean(read);
+};
+
+const addBookToLibrary = {
+    // do stuff here
+}
